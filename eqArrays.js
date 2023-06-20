a1b496585d67f486fe385fea7c2bed39d3360b9c
moogleKupo <tmaine@gmx.com>
2023-06-20 02:53:58 +0000

    eqArrays assignment week 1

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+function eqArray(numbers1, numbers2) {
+  if (numbers1.length !== numbers2.length) {
+    return false;
+  }
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] !== numbers2[i]){
+      return false;
+      }
+    }
+    return true;
+  }
+
+  //test
+console.log(eqArray([1,2,3], [1,2,3]));
