31f10ee9de97ec3aae390f2e9f52f973bab80e2d
moogleKupo <tmaine@gmx.com>
2023-06-20 18:36:32 +0000

    flatten assignment: learned about spread operator

--- flatten.js
+++ flatten.js
@@ -17,3 +17,15 @@ function eqArray(numbers1, numbers2) {
     }
     return true;
   }
+
+  let flatArray = [];
+  function flatten(arr) {
+    for (let i = 0; i < arr.length; i++){
+      if (Array.isArray(arr[i])){
+        flatArray.push(...arr[i]);
+      } else flatArray.push(arr[i]);      
+    }
+    return flatArray;
+  }
+  //test
+  console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
