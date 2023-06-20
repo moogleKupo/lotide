e259afa3c6e933cf045714fa2dd03fc31122acf1
moogleKupo <tmaine@gmx.com>
2023-06-20 18:14:27 +0000

    without assignment. Look at this again

--- without.js
+++ without.js
@@ -1,3 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.1/underscore-min.js"></script>
+
 function assertArraysEqual(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
@@ -17,3 +19,24 @@ function eqArray(numbers1, numbers2) {
     }
     return true;
   }
+
+  /* my attempt using _.without
+  the issue is that it requires the implementation of the Underscore library... 
+  not sure how to do that yet, but this function will work if the library is included.
+  */
+
+  function without(sourceArr, itemsToRemoveArr) {
+    resultArr = _.without(sourceArr, ...itemsToRemoveArr);
+    return resultArr;
+  }
+  // test
+  console.log(without([1,2,3,7,8,9], [3,7]));
+
+  //native javascript
+
+  function without(sourceArr, itemsToRemoveArr) {
+    return sourceArr.filter(item => !itemsToRemoveArr.includes(item));
+  }
+  //test
+  console.log(without([1,2,3,7,8,9], [3,7]));
+  
