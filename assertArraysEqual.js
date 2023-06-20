eb44779dc92e2d7c49536c31d2790700d0bedcc3
moogleKupo <tmaine@gmx.com>
2023-06-20 03:11:30 +0000

    assertArraysEqual assignment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,7 @@
-function eqArray(numbers1, numbers2) {
-  if (numbers1.length !== numbers2.length) {
-    console.log('false');
+function assertArraysEqual(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
   }
-  for (let i = 0; i < numbers1.length; i++) {
-    if (numbers1[i] !== numbers2[i]){
-      console.log('false');
-      } else {
-        console.log('true');
-      }
-    }
-  }
-  //test
+}
