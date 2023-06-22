5c0a535a38bff84c5129fb527c37e149175d8a55
moogleKupo <tmaine@gmx.com>
2023-06-22 20:30:21 +0000

    countOnly step

--- countOnly.js
+++ countOnly.js
@@ -17,3 +17,44 @@ function eqArray(numbers1, numbers2) {
     }
     return true;
   }
+
+  const countOnly = function(allItems, itemsToCount) {
+    results = {};
+    for (const item of allItems) {
+      console.log(item);
+    }
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+        if (itemsToCount[item]) {
+          if (results[item]) {
+            results[item] += 1;
+          } else {
+            results[item] = 1;
+          }
+          }
+        }
+    return results;
+  }
+  
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+  assertEqual(result1["Agouhanna"], undefined);
