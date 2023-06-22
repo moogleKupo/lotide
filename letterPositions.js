5a565fbade0ff8ab71e371f598295dc24a9bcf79
moogleKupo <tmaine@gmx.com>
2023-06-22 21:32:19 +0000

    letterPosition

---
+++ letterPositions.js
@@ -0,0 +1,8 @@
+const letterPositions = function(sentence) {
+  const results = {};
+const noSpaceSentence = sentence.replace(/\s/g, '');
+for (let letters of noSpaceSentence) {
+
+}
+  return results;
+}
