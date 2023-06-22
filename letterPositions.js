f0e430e0a78dd75de117da59cbc82a7b3330eaad
moogleKupo <tmaine@gmx.com>
2023-06-22 21:38:56 +0000

    countLetters complete

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,15 @@
 const letterPositions = function(sentence) {
-  const results = {};
+  const positions = {};
 const noSpaceSentence = sentence.replace(/\s/g, '');
-for (let letters of noSpaceSentence) {
-
+for (let i = 0; i < sentence.length -1; i++) {
+const letter = sentence[i];
+  if (i !== ' ') {
+      if (positions[letter]) {
+        positions[letter].push(i);
+      } else {
+        positions[letter] = [i];
+      }
+  }
 }
   return results;
 }
