8a408e504a3569420f863a6e5b848e0aa359b003
moogleKupo <tmaine@gmx.com>
2023-06-23 20:49:52 +0000

    eqObjects nonsensical at this point, come back to it

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,9 @@
+
+
+//variables
+const shirtObject = { color: "red", size: "medium"};
+const anotherShirtObject = {size: "medium", color: "red" };
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,6 +12,26 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqObjects = function(object1, object2){
+function eqArray(numbers1, numbers2) {
+  if (numbers1.length !== numbers2.length) {
+    return false;
+  }
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] !== numbers2[i]){
+      return false;
+      }
+    }
+    return true;
+  }
 
-}
+
+const eqObjects = function(object1, object2) {
+
+};
+console.log(eqObjects(shirtObject, anotherShirtObject));
+
+
+const longSleeveShirtObject = {size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject);
+
+console.log(assertEqual(shirtObject, anotherShirtObject));
