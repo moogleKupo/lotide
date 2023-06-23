3503c4886ae182ce0e8c7155a52c6ccf55788461
moogleKupo <tmaine@gmx.com>
2023-06-23 16:29:48 +0000

    letter positions

--- letterPositions.js
+++ letterPositions.js
@@ -1,15 +1,37 @@
+function eqArray(numbers1, numbers2) {
+  if (numbers1.length !== numbers2.length) {
+    return false;
+  }
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] !== numbers2[i]){
+      return false;
+      }
+    }
+    return true;
+  }
+
+  function assertArraysEqual(actual, expected) {
+    if (eqArray(actual, expected)) {
+      console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  }
+
 const letterPositions = function(sentence) {
   const positions = {};
-const noSpaceSentence = sentence.replace(/\s/g, '');
-for (let i = 0; i < sentence.length -1; i++) {
+  const noSpaceSentence = sentence.replace(/\s/g, '');
+    for (let i = 0; i < sentence.length; i++) {
 const letter = sentence[i];
-  if (i !== ' ') {
-      if (positions[letter]) {
-        positions[letter].push(i);
+      if (letter !== ' ') {
+          if (positions[letter]) {
+            positions[letter].push(i);
       } else {
         positions[letter] = [i];
       }
   }
 }
-  return results;
+  return positions;
 }
+
+assertArraysEqual(letterPositions("hello").e, [1]);
