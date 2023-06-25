d65f28221b4772981cf9373a3d9acbb98dc83d45
moogleKupo <tmaine@gmx.com>
2023-06-25 17:56:42 +0000

    building map function halfway

--- map.js
+++ map.js
@@ -1,7 +1,12 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
-const results = [];
+const map = function(array, cb) {
+  const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', cb(item));
+    console.log('---');
+  }
 return results;
 }
 
