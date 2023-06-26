92d93b864dc10236c15870903276a391c4e39b32
moogleKupo <tmaine@gmx.com>
2023-06-26 21:55:04 +0000

    added assertArraysEqual and eqArrays to file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,15 @@
+function eqArray(numbers1, numbers2) {
+  if (numbers1.length !== numbers2.length) {
+    return false;
+  }
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] !== numbers2[i]){
+      return false;
+      }
+    }
+    return true;
+  }
+
 function assertArraysEqual(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+function eqArray(numbers1, numbers2) {
+  if (numbers1.length !== numbers2.length) {
+    return false;
+  }
+  for (let i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] !== numbers2[i]){
+      return false;
+      }
+    }
+    return true;
+  }
+
+  function assertArraysEqual(actual, expected) {
+    if (eqArrays(actual, expected)) {
+      console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  }
+  
 const takeUntil = function(array, callback) {
 let newArray = [];
   for (item of array){
